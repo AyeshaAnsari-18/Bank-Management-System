69b5ddc5c9155a67078f961118ce1e6884fe64e0
Ayesha Ansari <itsa.ansari@gmail.com>
2024-11-08 01:36:04 +0500

    setdatabse

--- connection.php
+++ connection.php
@@ -1,8 +1,20 @@
 <?php      
-    $host = "localhost";  
-    $user = "root";  
-    $password = '';  
-    $db_name = "loginsignup";  
+    $host = "localhost";
+    $servername = "sql12.freesqldatabase.com";
+    $username = "sql12743380";
+    $password = "23Kb5fmbWA";
+    $dbname = "sql12743380";
+    $port = 3306;
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
       
     $conn = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
